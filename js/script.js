ba06d15d3f1c8c35c59ee926684a49144ecd5f8e
Garrett Barnard <garrettjbarnard@gmail.com>
2022-05-18 19:32:54 -0700

    adding more lines to js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ comment */
 
 console.log("Garrett Barnard");
 console.log("Favorite food is pizza.");
+console.log("Supercalifragilisticexpialidocious".length); //figure out the length of a string
+let int = 123
+console.log(int);
+console.log(" ^  " + typeof int);
+let largenumber = 12346546564564563421234n*12342343241342366423432434324234234n;
+console.log(largenumber);
+console.log(" ^  " + typeof largenumber);
